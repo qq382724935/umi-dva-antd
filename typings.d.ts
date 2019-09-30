0055cc8b78d9aecc80cd8e49dbc05f2f6d147f67
qq382724935 <382724935@qq.com>
2019-09-30 16:01:34 +0800

    Add files via upload

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,10 @@
+/*
+ * @Date: 2019-09-06 14:26:19
+ * @LastEditors: 刘利军
+ * @LastEditTime: 2019-09-30 10:02:41
+ */
 declare module '*.css';
 declare module '*.less';
 declare module "*.png";
+declare module "*.svg";
+declare const USE_MODE: string;
